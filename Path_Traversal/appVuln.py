9952ebd87b160d23d44b7f9800e4fbcbda585ff8
Abdarhman mohamed <abdarhman1337@gmail.com>
2026-08-19 14:46:43 +0300

    Update appVuln.py

--- Path_Traversal/appVuln.py
+++ Path_Traversal/appVuln.py
@@ -1,36 +1,40 @@
+from flask import Flask, request, send_file
 import os
-from flask import Flask, request, send_from_directory, abort
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# Ensure the directory exists so Flask doesn't throw an error
 FILES_DIR = os.path.join(BASE_DIR, "files")
-os.makedirs(FILES_DIR, exist_ok=True)
 
 @app.route("/")
 def home():
     return """
-    <h1>Fixed Path Traversal Lab</h1>
+    <h1>Vuln Path Traversal Lab</h1>
+
     <p>Try:</p>
+
     <ul>
-        <li><a href="/download?file=cat.jpg">cat image</a></li>
-        <li><a href="/download?file=dog.jpg">dog image</a></li>
+        <a href="/download?file=cat.jpg">cat image</a>
+        <p> </p>
+        <a href="/download?file=dog.jpg">dog image</a>
     </ul>
     """
 
+
 @app.route("/download")
 def download():
-    filename = request.args.get("file")
 
+    filename = request.args.get("file")
     if not filename:
         return "Missing file parameter", 400
 
+    path = os.path.join(FILES_DIR, filename)
+
     try:
-        # send_from_directory automatically blocks '..' and absolute path hacks
-        return send_from_directory(FILES_DIR, filename)
-    except ValueError:
-        return "File not found", 404
+        return send_file(path)
+
+    except FileNotFoundError:
+        return "file not found"
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(debug=False)
