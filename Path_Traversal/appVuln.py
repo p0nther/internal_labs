9757053cb70414bb449728f173fea632842c1b0c
Abdarhman mohamed <abdarhman1337@gmail.com>
2026-08-19 13:46:04 +0300

    Create appVuln.py

---
+++ Path_Traversal/appVuln.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, send_file
+import os
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FILES_DIR = os.path.join(BASE_DIR, "files")
+
+@app.route("/")
+def home():
+    return """
+    <h1>Vuln Path Traversal Lab</h1>
+
+    <p>Try:</p>
+
+    <ul>
+        <li>/download?file=cat.jpg</li>
+        <li>/download?file=dog.jpg</li>
+    </ul>
+    """
+
+@app.route("/download")
+def download():
+    filename = request.args.get("file")
+
+    if not filename:
+        return "Missing file parameter", 400
+
+    path = os.path.join(FILES_DIR, filename)
+
+    return send_file(path)
+
+if __name__ == "__main__":
+    app.run(debug=True)
