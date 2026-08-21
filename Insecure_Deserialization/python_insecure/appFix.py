ccfc28be1cc414c5871a3846c91fe304fd5a285a
Abdarhman mohamed <abdarhman1337@gmail.com>
2026-08-21 03:00:37 +0300

    Add files via upload

---
+++ Insecure_Deserialization/python_insecure/appFix.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template_string
+import json
+
+app = Flask(__name__)
+
+HTML = """
+<h1>Secure Deserialization Lab</h1>
+
+<form method="POST">
+    <textarea name="data" rows="10" cols="80"></textarea><br><br>
+    <button type="submit">Import Profile</button>
+</form>
+
+{% if result %}
+<hr>
+<h3>Parsed Data:</h3>
+<pre>{{ result }}</pre>
+{% endif %}
+"""
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = ""
+
+    if request.method == "POST":
+        try:
+            data = request.form.get("data", "")
+
+            # SAFE
+            obj = json.loads(data)
+
+            result = repr(obj)
+
+        except Exception as e:
+            result = f"Error: {e}"
+
+    return render_template_string(HTML, result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5001)
